30590856084d007b1e17e721c0df51f26203917c
GalkinAlex1503 <galkin.alexandr1503@gmail.com>
2024-09-15 01:21:33 +0200

    feat: add init variables

--- src/main/kotlin/com/galkinalex/kotlincourse/lesson4/Main.kt
+++ src/main/kotlin/com/galkinalex/kotlincourse/lesson4/Main.kt
@@ -3,42 +3,122 @@ package com.galkinalex.kotlincourse.lesson4
 //Для каждого значения допиши подходящий тип данных
 
 //42  Int
+var test1: Int = 42
+
 //98765432123456789L Long
+var test2: Long = 98765432123456789L
+
 //23.45f  Float
+var test3: Float = 23.45f
+
 //0.123456789 Double
+var test4: Double = 0.123456789
+
 //"Kotlin & Java"  String
+var test5: String = "Kotlin & Java"
+
 //false  Boolean
+var test6: Boolean = false
+
 //'c'  Char
+var test7: Char = 'c'
+
 //500  Int
+var test8: Int = 500
+
 //4294967296L Long
+var test9: Long = 4294967296L
+
 //18.0f  Float
-//-0.001   Int
+var test10: Float = 18.0f
+
+
+//-0.001   Double
+var test11: Double = -0.001
+
 //"OpenAI"  String
+var test12: String = "OpenAI"
+
 //“true”  Error type
+
 //'9'  Char
+var test13: Char = '9'
+
 //2048  Int
+var test14: Int = 2048
+
 //10000000000L  Long
+var test15: Long = 10000000000L
+
 //5.75f  Float
+var test16: Float = 5.75f
+
 //1.414  Double
+var test17: Double = 1.414
+
+
 //"Artificial Intelligence"   String
+var test18: String = "Artificial Intelligence"
+
 //false   Boolean
+var test19: Boolean = false
+
 //'@'  Char
+var test20: Char = '@'
+
 //1024 Int
+var test21: Int = 1024
+
 //1234567890123L Long
+var test22: Long = 1234567890123L
+
 //10.01f  Float
-//-273.15  Int
+var test23: Float = 10.01f
+
+//-273.15  Double
+var test24: Double = -273.15
+
 //"SpaceX"  String
+var test25: String = "SpaceX"
+
 //true   Boolean
+var test26: Boolean = true
+
+
 //“🤯”  String
+// var test27: String = “🤯”  ???
+
 //‘65535’   Error type
+
 //72057594037927935L   Long
+var test27: Long = 72057594037927935L
+
 //2.71828f  Float
+var test28: Float = 2.71828f
+
 //101.0101  Double
+var test29: Double = 101.0101
+
 //"Quantum Computing"  String
+var test30: String = "Quantum Computing"
+
 //false   Boolean
+var test31: Boolean = false
+
 //'x'  Char
+var test32: Char = 'x'
+
 //314  Int
+var test33: Int = 314
+
 //123456789123456789L  Long
+var test34: Long = 123456789123456789L
+
 //6.626f  Float
+var test35: Float = 6.626f
+
 //0.007  Double
+var test36: Double = 0.007
+
 //"Android Studio"  String
+var test37: String = "Android Studio"

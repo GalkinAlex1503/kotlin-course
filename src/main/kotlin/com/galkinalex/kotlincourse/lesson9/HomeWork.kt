59b5d017fa8c648ccfb722dacca9b61cfb7ed8de
GalkinAlex1503 <galkin.alexandr1503@gmail.com>
2024-10-04 12:35:50 +0200

    fix: fix getMonth

--- src/main/kotlin/com/galkinalex/kotlincourse/lesson9/HomeWork.kt
+++ src/main/kotlin/com/galkinalex/kotlincourse/lesson9/HomeWork.kt
@@ -6,10 +6,13 @@ fun main() {
     var curDay = "01.10.2024"
     println(calcDay(curDay, bBay))
 
-    curDay = "01.11.2024"
+    curDay = "01.12.2024"
     println(calcDay(curDay, bBay))
 
-    curDay = "01.11.2024"
+    curDay = "01.03.2024"
+    println(calcDay(curDay, bBay))
+
+    curDay = "02.10.2024"
     println(calcDay(curDay, bBay))
 }
 
@@ -19,7 +22,7 @@ fun calcDay(day: String, bDay: String): String {
     } else {
         val monthToDay = getMonth(day, bDay)
         return  if (monthToDay == 0) {
-            " День рождения уже скоро!"
+            "День рождения уже скоро!"
         } else {
             "Сегодня не тот день! Ждите еще $monthToDay месяцев"
         }
@@ -31,7 +34,25 @@ fun checkDay(day: String, bDay: String): Boolean {
 }
 
 fun getMonth(day: String, bDay: String): Int {
-    var result = 0;
+    val dayList = day.split(".")
+    val bDayList = bDay.split(".")
+    val month = dayList[1].toInt()
+    val bMonth = bDayList[1].toInt()
+    val curDay = dayList[0].toInt()
+    val dayB = bDayList[1].toInt()
 
-    return result
+    return if (month == bMonth) {
+        if (curDay < dayB) {
+            0
+        } else  {
+            12
+        }
+    } else {
+        if (month < bMonth) {
+            bMonth - month
+        } else  {
+            (12 - month) + bMonth
+        }
+    }
+
 }

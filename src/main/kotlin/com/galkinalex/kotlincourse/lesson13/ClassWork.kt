070d51e4987675bacb4834c56247db35fc104d05
GalkinAlex1503 <galkin.alexandr1503@gmail.com>
2024-10-22 13:04:45 +0200

    init: lesson13

---
+++ src/main/kotlin/com/galkinalex/kotlincourse/lesson13/ClassWork.kt
@@ -0,0 +1,14 @@
+package com.galkinalex.kotlincourse.lesson13
+
+fun main() {
+
+
+    val list = listOf(8, 56, 23, 87, 12, 18, 11)
+
+
+    val filtered2 = list.filter { it in 7..17 }
+    println(filtered2)
+
+
+
+}

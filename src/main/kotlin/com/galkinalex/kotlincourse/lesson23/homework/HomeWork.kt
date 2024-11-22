42527977780401669348988aa185f2c97e45d423
GalkinAlex1503 <galkin.alexandr1503@gmail.com>
2024-11-22 15:48:59 +0100

    feat: add all tasks

--- src/main/kotlin/com/galkinalex/kotlincourse/lesson23/homework/HomeWork.kt
+++ src/main/kotlin/com/galkinalex/kotlincourse/lesson23/homework/HomeWork.kt
@@ -1,5 +1,8 @@
 package com.galkinalex.kotlincourse.lesson23.homework
 
+import com.galkinalex.kotlincourse.lesson3.contactInfo
+import kotlin.math.pow
+
 //Задача 1
 //Напишите функцию analyzeDataType, принимающую параметр типа Any. Функция должна определить тип аргумента и вывести соответствующее сообщение:
 //  Для строки: "Это строка: [значение]".
@@ -39,6 +42,50 @@ fun getStringLengthOrZero(param: Any?): Int {
     return (param as? String)?.length ?: 0
 }
 
+//Задача 4
+//Создайте функцию, которая принимает параметр типа Any.
+//Функция гарантированно ожидает число (в виде числа или строки, например 4 или 4.2 или “4.2”)
+// и должна вернуть квадрат этого числа. Если придёт число, его нужно возвести в квадрат, если придёт строка,
+// то его нужно преобразовать в число через функцию toDouble() и возвести в квадрат.
+
+fun calcSquare(num: Any): Double {
+    return if (num is Double) {
+        num * num
+    } else {
+        (num as String).toDouble() * (num as String).toDouble()
+    }
+}
+
+//Задача 5
+//Напишите функцию sumIntOrDoubleValues, которая принимает список элементов типа Any 
+// и возвращает сумму всех целочисленных (Int) и вещественных (Double) значений в списке. 
+// Все остальные типы должны быть проигнорированы.
+
+fun sumIntOrDoubleValues(listElem: List<Any>): Double {
+    var result = 0.0
+    listElem.forEach {
+        when (it) {
+            is Int -> result += it.toDouble()
+            is Double -> result += it
+            else -> result = result // return@forEach
+        }
+    }
+    return result
+}
+
+//Задача 6
+//Создайте функцию tryCastToListAndPrint, которая принимает параметр типа Any и пытается привести его к типу List<*>.
+// Если приведение успешно, функция должна напечатать все строки из списка,
+// если элемент не является строкой то вывести предупреждение об этом.
+// Если приведение неудачно, должно быть выведено сообщение об ошибке, не прерывая выполнение программы.
+
+fun tryCastToListAndPrint(param: Any) {
+    (param as List<*>).forEach {
+        val elem = it as? String
+        println(elem ?: "$it not a String")
+    }
+}
+
 fun main() {
     println("Task 1")
     analyzeDataType("sdfsd")
@@ -53,6 +100,19 @@ fun main() {
     println("Int ->  ${safeCastToList(13)}")
 
     println("Task 3")
+    println("String -> ${getStringLengthOrZero("sdfsdf")}")
+    println("null -> ${getStringLengthOrZero(null)}")
+    println("Int -> ${getStringLengthOrZero(13)}")
 
+    println("Task 4")
+    println("2.5 -> ${calcSquare(2.5)}")
+    println("\"2.5\" -> ${calcSquare("2.5")}")
+
+    println("Task 5")
+    val listOne = listOf("asda", 1, 2.0)
+    println("$listOne -> ${sumIntOrDoubleValues(listOne)}")
+
+    println("Task 6")
+    tryCastToListAndPrint(listOf("sdfs","fghfg", 13))
 
 }

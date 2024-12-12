5f9543ece1f9fc228001fc156f3e13a5e2ad11ae
GalkinAlex1503 <galkin.alexandr1503@gmail.com>
2024-12-12 21:04:07 +0100

    feat: add Task4

---
+++ src/main/kotlin/com/galkinalex/kotlincourse/lesson28/homework/Task4.kt
@@ -0,0 +1,25 @@
+package com.galkinalex.kotlincourse.lesson28.homework
+
+import java.io.File
+
+//Задача 4
+//Создайте директорию workspace/task4/temp.
+//Внутри директории temp создайте несколько вложенных файлов и директорий.
+//Удалите директорию workspace/task4 со всем содержимым
+
+fun main() {
+    val file = File(ConstHomeWork28.WORKSPACE + "/task4/temp")
+    file.mkdirs()
+    println("create files system")
+    for (i in 1..5) {
+        val tempDir = File(file.path).resolve(i.toString())
+        tempDir.mkdirs()
+        for (j in 1..3) {
+            val tempFile  = File(tempDir.path).resolve("$j.txt")
+            tempFile.createNewFile()
+        }
+    }
+
+    println("delete files system ${file.path}")
+    file.parentFile.deleteRecursively()
+}
